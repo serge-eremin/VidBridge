ed4d216113c1aa9153e2bfc534512af1fef89a4f
Sergey <velesinc@gmail.com>
2026-02-18 18:52:45 -0500

    db config implemented

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
 
 import { ENV } from './config/env.js'
@@ -6,11 +7,11 @@ import { connectDB } from './config/db.js'
 
 const app = express()
 
+app.use(clerkMiddleware())
+
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
-
-console.log('mongo_uri-:', ENV.MONGO_URI)
 
 app.listen(ENV.PORT, () => {
   console.log(`Server started on port: ${ENV.PORT}`)
