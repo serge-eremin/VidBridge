21a9c3f81865941b0d6a7e6853b62521e3e9d42a
Sergey <velesinc@gmail.com>
2026-02-19 21:04:39 -0500

    auth page implemented

---
+++ web/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import {} from 'react'
+import { UserButton } from '@clerk/clerk-react'
+
+export const HomePage = () => {
+  return (
+    <div>
+      <UserButton />
+      HomePage
+    </div>
+  )
+}
